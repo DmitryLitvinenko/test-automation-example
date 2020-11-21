f833f879a88df1288284d02bc3275e0f61bbc1a8
dmitryl <dmitrijs.litvinenko@neotech.lv>
2020-11-21 22:31:33 +0200

    adding: TestResultLoggerExtension.java

--- src/test/java/com/dmitry/litvinenko/cases/FirstTest.java
+++ src/test/java/com/dmitry/litvinenko/cases/FirstTest.java
@@ -1,10 +1,13 @@
 package com.dmitry.litvinenko.cases;
 
+import com.dmitry.litvinenko.cases.util.TestResultLoggerExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static com.codeborne.selenide.Selenide.open;
 
-public class  FirstTest {
+@ExtendWith(TestResultLoggerExtension.class)
+public class FirstTest {
 
     @Test
     public void openGoogleTest() {
